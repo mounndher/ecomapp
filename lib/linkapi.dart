5f1ae050bdd7ead5e52aa8046b6186bbfaada32d
unknown <nanou.riache19@gmail.com>
2023-08-03 16:30:57 +0100

    first committ

--- lib/linkapi.dart
+++ lib/linkapi.dart
@@ -2,7 +2,12 @@ class AppLink {
   static const String server = "https://mondherjayh.000webhostapp.com/";
   static const String test = "$server/test.php";
   //static const String test = "http://www.portjob.eb2a.com/test.php";
+  //images
+  static const String image = "https://mondherjayh.000webhostapp.com/upload";
+  static const String imagecategories = "$image/categories";
+  static const String imageitems = "$image/items";
   //auth
+  static const String homepage = "$server/auth/home.php";
   static const String signUp = "$server/auth/signup.php";
   static const String login = "$server/auth/login.php";
   static const String verifycodesignup = "$server/auth/verifycode.php";
